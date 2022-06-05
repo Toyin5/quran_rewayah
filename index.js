41a5f0744be31335ec8fd1fc5f310ae12f273413
saifuddien <ariefsaifuddien01@gmail.com>
2022-06-05 19:09:16 +0200

    ok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
-import expressLayouts from "express-ejs-layouts"
+// import expressLayouts from "express-ejs-layouts"
 import { quranRouters } from "./routers/quran.js"
 import database from "./utils/db.js"
 import 'dotenv/config'
@@ -11,28 +11,35 @@ const port = 3300 || process.env.PORT
 
 database()
 app.use(cors())
-app.use(expressLayouts)
+// app.use(expressLayouts)
 app.use(bodyParser.json())
-app.use('/public', express.static('public'))
-app.set('view engine', 'ejs')
+// app.use('/public', express.static('public'))
+// app.set('view engine', 'ejs')
 
 app.use('/api-quran-rewayah', quranRouters)
 
-app.get('/', (req, res) => {
-  res.render('home', {
-    title: 'Home',
-    layout: './layout/layout',
-    data: 'Home Page'
+app.use('/:any', (req, res) => {
+  res.status(404).json({
+    code: 404,
+    message: req.params.any + ' Not Found'
   })
 })
 
-app.use('/:any', (req, res) => {
-  res.render('404', {
-    title: '404',
-    layout: './layout/layout',
-    data: req.params.any + ' Page Not Found'
-  })
-})
+// app.get('/', (req, res) => {
+//   res.render('home', {
+//     title: 'Home',
+//     layout: './layout/layout',
+//     data: 'Home Page'
+//   })
+// })
+
+// app.use('/:any', (req, res) => {
+//   res.render('404', {
+//     title: '404',
+//     layout: './layout/layout',
+//     data: req.params.any.toUpperCase() + ' Page Not Found'
+//   })
+// })
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
