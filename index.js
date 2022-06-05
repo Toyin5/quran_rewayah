d1f485a073d6f1df4001ff3ae539592ee634cea7
saifuddien <ariefsaifuddien01@gmail.com>
2022-06-05 17:51:02 +0200

    ejs layout

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
+import expressLayouts from "express-ejs-layouts"
 import { quranRouters } from "./routers/quran.js"
 import database from "./utils/db.js"
 
@@ -9,25 +10,29 @@ const port = 3300
 
 database()
 app.use(cors())
+app.use(expressLayouts)
 app.use(bodyParser.json())
+app.use('/public', express.static('public'))
+app.set('view engine', 'ejs')
 
-app.use('/api/quran-rewayah', quranRouters)
+app.use('/api-quran-rewayah', quranRouters)
 
-app.use('/', (req, res) => {
-  res.status(200).json({
-    code: 200,
-    message: 'Wellcome to our API'
+app.get('/', (req, res) => {
+  res.render('home', {
+    title: 'Home',
+    layout: './layout/layout',
+    data: 'Home Page'
   })
 })
 
-app.use('/:any', (req, res) => {
-  res.status(404).json({
-    code: 404,
-    message: req.params.any + ' Page Not Found'
+app.use('/', (req, res) => {
+  res.render('404', {
+    title: '404',
+    layout: './404',
+    data: '404 Page'
   })
 })
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
-
