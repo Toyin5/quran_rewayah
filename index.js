2dea17e9cf3ee2b71a87cb0b449cd47bcaa04ff7
saifuddien <ariefsaifuddien01@gmail.com>
2022-06-05 19:51:11 +0200

    ok

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.use(bodyParser.json())
 app.use('/public', express.static('public'))
 app.set('view engine', 'ejs')
 
-app.use('/', (req, res, next) => {
+app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
-    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+    "default-src 'self';"
   );
   next();
 });
