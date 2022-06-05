ecff2839f629acaedc448709d16ea51a2a1ea384
saifuddien <ariefsaifuddien01@gmail.com>
2022-06-05 19:21:16 +0200

    ejs

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import expressLayouts from "express-ejs-layouts"
 import { quranRouters } from "./routers/quran.js"
 import database from "./utils/db.js"
 import 'dotenv/config'
+import 'ejs'
 
 const app = express()
 const port = process.env.PORT || 3300
@@ -13,7 +14,6 @@ database()
 app.use(cors())
 app.use(expressLayouts)
 app.use(bodyParser.json())
-app.use('/public', express.static('public'))
 app.set('view engine', 'ejs')
 
 app.use('/api-quran-rewayah', quranRouters)
