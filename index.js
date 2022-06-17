71dcf3443f02258ea0493fed53ef761b2b722b7e
saifuddien <ariefsaifuddien01@gmail.com>
2022-06-17 21:02:57 +0200

    return

--- index.js
+++ index.js
@@ -25,42 +25,42 @@ app.use('/api/admin', adminRouters)
 app.use('/api/users', usersRouters)
 app.use('/api/quraa', quraaRouters)
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({
-//     code: 200,
-//     message: 'Wrong Endpoint, Try the endpoints below!',
-//     github: 'https://github.com/saifuddien/quran_rewayah',
-//     docs: [
-//       {
-//         desc: 'Getting One Juz Of Quran.',
-//         url: {
-//           endpoint: '/api/quran-rewayah/juz/{number of juz}',
-//           example: 'https://quran-rewayah-api.vercel.app/api/quran-rewayah/juz/2'
-//         }
-//       },
-//       {
-//         desc: 'Getting All Surah Of Quran.',
-//         url: {
-//           endpoint: '/api/quran-rewayah/surah',
-//           example: 'https://quran-rewayah-api.vercel.app/api/quran-rewayah/surah'
-//         }
-//       },
-//       {
-//         desc: 'Getting One Surah Of Quran.',
-//         url: {
-//           endpoint: '/api/quran-rewayah/surah/{number of surah}',
-//           example: 'https://quran-rewayah-api.vercel.app/api/quran-rewayah/surah/5'
-//         }
-//       },
-//       {
-//         desc: 'Getting One Ayah Of Surah in Quran.',
-//         url: {
-//           endpoint: '/api/quran-rewayah/ayah/{number of ayah}?surah={number of surah}',
-//           example: 'https://quran-rewayah-api.vercel.app/api/quran-rewayah/ayah/5?surah=12'
-//         }
-//       }
-//     ]
-//   })
-// })
+app.use('/', (req, res) => {
+  res.status(200).json({
+    code: 200,
+    message: 'Wrong Endpoint, Try the endpoints below!',
+    github: 'https://github.com/saifuddien/quran_rewayah',
+    docs: [
+      {
+        desc: 'Getting One Juz Of Quran.',
+        url: {
+          endpoint: '/api/quran-rewayah/juz/{number of juz}',
+          example: 'https://quran-rewayah-api.vercel.app/api/quran-rewayah/juz/2'
+        }
+      },
+      {
+        desc: 'Getting All Surah Of Quran.',
+        url: {
+          endpoint: '/api/quran-rewayah/surah',
+          example: 'https://quran-rewayah-api.vercel.app/api/quran-rewayah/surah'
+        }
+      },
+      {
+        desc: 'Getting One Surah Of Quran.',
+        url: {
+          endpoint: '/api/quran-rewayah/surah/{number of surah}',
+          example: 'https://quran-rewayah-api.vercel.app/api/quran-rewayah/surah/5'
+        }
+      },
+      {
+        desc: 'Getting One Ayah Of Surah in Quran.',
+        url: {
+          endpoint: '/api/quran-rewayah/ayah/{number of ayah}?surah={number of surah}',
+          example: 'https://quran-rewayah-api.vercel.app/api/quran-rewayah/ayah/5?surah=12'
+        }
+      }
+    ]
+  })
+})
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
